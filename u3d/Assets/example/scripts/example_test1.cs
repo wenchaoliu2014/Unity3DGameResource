28b38a6d8752ec0f4938d7f1137017660b643cd5
wenchaoliu <wenchaoliu@foxmail.com>
2020-06-08 15:39:54 +0800

    test

--- u3d/Assets/example/scripts/example_test1.cs
+++ u3d/Assets/example/scripts/example_test1.cs
@@ -8,8 +8,8 @@ namespace example.scripts
 {
     public class example_test1 : MonoBehaviour
     {
-        AbRequest _req = null;
         private readonly List<string> _paths = new List<string>();
+        private AbRequest _req;
 
         private void Start()
         {
@@ -32,38 +32,31 @@ namespace example.scripts
         private void finish_callback(Dictionary<string, AssetBundle> res)
         {
             var sb = new StringBuilder();
-            for (var i = 0; i < _paths.Count; i++)
+            foreach (var t in _paths)
             {
                 sb.Clear();
-                var index = _paths[i].LastIndexOf("/", StringComparison.Ordinal);
-                sb.Append(_paths[i].Substring(index + 1));
-                var prefab = res[_paths[i]].LoadAsset<GameObject>(sb.ToString());
+                var index = t.LastIndexOf("/", StringComparison.Ordinal);
+                sb.Append(t.Substring(index + 1));
+                var prefab = res[t].LoadAsset<GameObject>(sb.ToString());
                 Instantiate(prefab);
             }
         }
 
-        private void error_callback(string path, string error)
+        private static void error_callback(string path, string error)
         {
             Debug.LogError(" path :" + path + "\nerror :" + error);
         }
 
         // Update is called once per frame
-        private void Update()
-        {
-            //
-        }
+//        private void Update()
+//        {
+//        }
 
         private void OnGUI()
         {
-            if (this._req != null)
-            {
-                GUI.Label(new Rect(0, 0, 100, 40), "Progress " + _req.Progress);
-            }
+            if (_req != null) GUI.Label(new Rect(0, 0, 100, 40), "Progress " + _req.Progress);
 
-            if (GUI.Button(new Rect(30, 50, 50, 50), "clear"))
-            {
-                this._req.Disport();
-            }
+            if (GUI.Button(new Rect(30, 50, 50, 50), "clear")) _req.Disport();
         }
     }
 }
